a65fb04be4f641ea3cf19fbd1b6dd5725effd405
Bartlomiej Duda <ikskoks@gmail.com>
2019-05-30 23:40:18 +0200

    Corrected prolog script 2

--- Paradygmaty i języki programowania/Prolog_02_skrypt.pl
+++ Paradygmaty i języki programowania/Prolog_02_skrypt.pl
@@ -29,6 +29,23 @@ multiply([X|Xs], [Y|Ys], L) :-
 razy(A, [X], [Z]) :- Z is A*X.
 razy(A, [X|T], L) :- Z is A*X, L=[Z|Ls], razy(A, T, Ls).
 
+
+% zad 7
 suma([],0).
 suma([X|Xs],S) :- suma(Xs,S1), S is X+S1. 
 
+list_length([], 0).
+list_length([_|Xs], L) :- list_length(Xs,N), L is N+1 .
+
+
+
+% inne 
+is_on_list(X, [X|Y]).
+
+is_on_list2(X, [X|Y]).
+is_on_list2(X, [Z|Y]) :- is_on_list2(X, Y).
+
+join_lists(X, Y, Z) :- append(X, Y, Z) .
+join_lists2([X|Y],List2,[X|Result]):- append(Y,List2,Result).
+
+is_member2(X, [Y|T]) :- X = Y; member(X, T).
